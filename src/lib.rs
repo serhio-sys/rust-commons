bfb0dbce79085836eb0d170859ef738218a8903b
serhio-sys <serhii.kolodyazhniy@gmail.com>
2024-12-15 12:36:10 +0100

    Try to use extern crate

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,12 @@ pub mod config;
 pub mod database;
 pub mod crypto;
 
-pub use diesel_migrations;
-pub extern crate diesel;
-
 pub use log;
 pub use dotenvy;
 pub use lazy_static;
-pub use chrono;
 pub use uuid;
 pub use rand;
-pub use thiserror;
-pub extern crate base64;
-pub extern crate pwhash;
 
-pub use serde;
-pub use serde_json;
-pub use serde_qs;
-pub use serde_urlencoded;
+pub extern crate pwhash;
+pub extern crate base64;
+pub extern crate diesel;
