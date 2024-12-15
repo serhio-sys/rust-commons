be3347da9f0d39d2ae4c40ed09f5d6c46597f494
serhio-sys <serhii.kolodyazhniy@gmail.com>
2024-12-15 12:56:09 +0100

    Removed serialization crates

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,8 @@ pub use dotenvy;
 pub use lazy_static;
 pub use uuid;
 pub use rand;
-pub use serde_json;
-pub use serde_qs;
-pub use serde_urlencoded;
 
 pub extern crate thiserror;
-pub extern crate serde_json;
-pub extern crate serde_qs;
-pub extern crate serde_urlencoded;
 pub extern crate pwhash;
 pub extern crate base64;
 pub extern crate diesel;
