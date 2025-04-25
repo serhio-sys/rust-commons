6bc0151fac26f5675555023eb7b47080a56aab25
Serhii Kolodiazhnyi <serhii.kolodyazhniy@gmail.com>
2025-04-25 13:57:51 +0200

    Added Clone derive to Language enum

--- src/i18n/locales.rs
+++ src/i18n/locales.rs
@@ -1,7 +1,7 @@
 use serde::{ Deserialize, Serialize };
 use strum_macros::{ EnumString, Display };
 
-#[derive(EnumString, Display, Serialize, Deserialize, Clone, PartialEq, Eq, Hash)]
+#[derive(EnumString, Display, Serialize, Deserialize, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Language {
     #[strum(serialize = "en")] English,
     #[strum(serialize = "es")] Spanish,
